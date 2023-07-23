8b0459fdb3c6e26a845815483088062bf40224d3
Aleksey Petkun <aleksey_2710@mail.ru>
2023-07-23 23:06:52 +0300

    initial commit

---
+++ src/main/java/pro/sky/aleksfirstbotapp/AleksFirstBotAppApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.aleksfirstbotapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AleksFirstBotAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AleksFirstBotAppApplication.class, args);
+    }
+
+}
