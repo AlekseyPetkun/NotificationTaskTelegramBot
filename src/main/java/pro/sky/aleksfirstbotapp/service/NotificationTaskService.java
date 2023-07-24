0b49b58d22de569c34282e36fa133bf976a092c7
Aleksey Petkun <aleksey_2710@mail.ru>
2023-07-24 23:14:12 +0300

    добавление документации

--- src/main/java/pro/sky/aleksfirstbotapp/service/NotificationTaskService.java
+++ src/main/java/pro/sky/aleksfirstbotapp/service/NotificationTaskService.java
@@ -1,17 +1,15 @@
 package pro.sky.aleksfirstbotapp.service;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import pro.sky.aleksfirstbotapp.entity.NotificationTask;
 import pro.sky.aleksfirstbotapp.repository.NotificationTaskRepository;
 
 @Service
+@AllArgsConstructor
 public class NotificationTaskService {
 
     private final NotificationTaskRepository notificationTaskRepository;
-
-    public NotificationTaskService(NotificationTaskRepository notificationTaskRepository) {
-        this.notificationTaskRepository = notificationTaskRepository;
-    }
 
     public void save(NotificationTask notificationTask) {
 
