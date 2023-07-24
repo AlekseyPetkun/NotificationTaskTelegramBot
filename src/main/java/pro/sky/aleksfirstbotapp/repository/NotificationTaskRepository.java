6f3ad52e0c716f6f0913cbf05d49d78ebefd396c
Aleksey Petkun <aleksey_2710@mail.ru>
2023-07-24 23:14:35 +0300

    добавление документации

--- src/main/java/pro/sky/aleksfirstbotapp/repository/NotificationTaskRepository.java
+++ src/main/java/pro/sky/aleksfirstbotapp/repository/NotificationTaskRepository.java
@@ -9,19 +9,28 @@ import pro.sky.aleksfirstbotapp.entity.NotificationTask;
 import java.time.LocalDateTime;
 import java.util.List;
 
+/**
+ * Репозиторий напоминаний
+ */
 public interface NotificationTaskRepository extends JpaRepository<NotificationTask, Long> {
 
+    /**
+     * Поиск всех напоминаний по дате и времени
+     *
+     * @param localDateTime дата и время
+     * @return список напоминаний
+     */
     List<NotificationTask> findAllByNotificationDateTime(LocalDateTime localDateTime);
 
-    List<NotificationTask> findAllByNotificationDateTimeAndChatId(LocalDateTime localDateTime, long chatId);
+    /*List<NotificationTask> findAllByNotificationDateTimeAndChatId(LocalDateTime localDateTime, long chatId);
 
     @Modifying
     @Query("DELETE FROM NotificationTask WHERE message like %:nameLike%")
     void removeAllLike(@Param("nameLike") String nameLike);
 
-//    @Query("SELECT nt FROM NotificationTask nt WHERE nt.user.name like %:nameLike%")
+    @Query("SELECT nt FROM NotificationTask nt WHERE nt.user.name like %:nameLike%")
 
-//    @Query("SELECT nt.* FROM notification_tasks nt INNER JOIN user u ON nt.user_id= u.id WHERE u.name like %:nameLike%", nativeQuery = true)
-//    List<NotificationTask> findAllByNameLike(@Param("nameLike") String nameLike);
-
+    @Query("SELECT nt.* FROM notification_tasks nt INNER JOIN user u ON nt.user_id= u.id WHERE u.name like %:nameLike%", nativeQuery = true)
+    List<NotificationTask> findAllByNameLike(@Param("nameLike") String nameLike);
+*/
 }
