ddc78231bda199bbd9bfb95e03f0f46459156547
Aleksey Petkun <aleksey_2710@mail.ru>
2023-07-24 23:12:38 +0300

    добавление аннотации

--- src/main/java/pro/sky/aleksfirstbotapp/AleksFirstBotAppApplication.java
+++ src/main/java/pro/sky/aleksfirstbotapp/AleksFirstBotAppApplication.java
@@ -2,12 +2,13 @@ package pro.sky.aleksfirstbotapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class AleksFirstBotAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AleksFirstBotAppApplication.class, args);
     }
-
 }
